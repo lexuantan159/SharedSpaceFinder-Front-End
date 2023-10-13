90bea72c221e8ebcf301f4cf6d70e64d45dbf733
Xuan Hoang <xuanh6555@gmail.com>
2023-10-13 20:46:46 +0700

    Pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { Home, Spaces, LogIn, Register ,SpaceDetail, Contact } from '../pages/index';
+import { Home, Spaces, LogIn, Register ,SpaceDetail, Contact, Profile, PostSpace, Messenge, ManagePost, FavoriteSpace } from '../pages/index';
 const router = [
     { path: '/', component: Home },
     { path: '/spaces', component: Spaces },
@@ -6,6 +6,11 @@ const router = [
     { path: '/login', component: LogIn },
     { path: '/register', component: Register },
     { path: '/spaces/:spaceId', component: SpaceDetail },
-];
+    { path: '/profile', component: Profile },
+    { path: '/postspaces', component: PostSpace },
+    { path: '/messenge', component: Messenge },
+    { path: '/managepost', component: ManagePost },
+    { path: '/favoritespace', component: FavoriteSpace },
+];  
 
 export {router}
