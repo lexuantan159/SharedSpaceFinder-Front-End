a99b6f2f16d403801a066e2b659f4300049327b2
Xuan Hoang <xuanh6555@gmail.com>
2023-11-05 13:44:21 +0700

    UI Post Space

--- src/pages/postspaces/PostSpaceHome.jsx
+++ src/pages/postspaces/PostSpaceHome.jsx
@@ -1,47 +1,76 @@
-import React from "react";
-import AddressPost from "./AddressPost";
-import Overview from "./Overview";
+import React, { useState } from "react";
+
 import { HiOutlineCamera } from "react-icons/hi";
 import MapBox from "../../components/map/MapBox";
+import AddressPost from "../../components/addresspost/AddressPost";
+import Overview from "../../components/overview/Overview";
+import validate from "../../components/inputform/ValidateFields";
+
+
 
 const PostSpaceHome = () => {
+  const [payload, setPayload] = useState({
+    category_id: "",
+    title: "",
+    description: "",
+    price: "",
+    area: "",
+    people_numbers: 0,
+    bathroom_numbers: 0,
+    bedroom_numbers: 0,
+    address: "",
+    image: "",
+  });
+  const [invalidFields, setInvalidFields] = useState([])
+  
+  const handleSubmit = async () => {
+
+
+  console.log(payload);
+  const result = validate(payload,setInvalidFields)
+  console.log(result)
+  }
   return (
     <div className="px-6">
-      <h1 className="text-3x1 text-primaryColor text-xl font-medium py-4 border-b border-gray-200">
+      <h1 className="text-3x1 border-b border-gray-200 py-4 text-xl font-medium text-primaryColor">
         {" "}
         Đăng Tin Cho Thuê
       </h1>
       <div className="flex gap-4">
-        <div className="py-4 flex flex-col gap-8 flex-auto">
+        <div className="flex flex-auto flex-col gap-8 py-4">
           <AddressPost />
-          <Overview />
-          <div className="w-full mg-6">
-            <h2 className="font-semibold text-xl py-4">Hình ảnh</h2>
+          <Overview invalidFields={invalidFields} setInvalidFields={setInvalidFields} payload={payload} setPayload={setPayload} />
+          <div className="mg-6 w-full">
+            <h2 className="py-4 text-xl font-semibold">Hình ảnh</h2>
             <small>Cập nhật hình ảnh rõ ràng </small>
             <div className="w-full">
               <label
-                className="w-full border h-[200px] my-4 gap-2 flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-md"
+                className="my-4 flex h-[200px] w-full flex-col items-center justify-center gap-2 rounded-md border border-2 border-dashed border-gray-300"
                 htmlFor="file"
               >
                 <HiOutlineCamera size={50} />
                 Thêm ảnh
               </label>
-              <input hidden type="file" id="file" />
+              <input hidden type="file" id="file" multiple />
             </div>
           </div>
 
-          <button className="w-full px-2 py-2 text-white bg-green-600 rounded-md hover:underline">
+          <button className="w-full rounded-md bg-green-600 px-2 py-2 text-white hover:underline" 
+          onClick={handleSubmit}
+         
+          >
             Tiếp Tục
+            
           </button>
         </div>
 
-        <div className="w-[30%] flex-none">
-          <div className="py-10 h-[350px]">
+        <div className="h-full w-[30%] flex-none">
+          <div className="py-10 lg:h-full ">
             <MapBox />
           </div>
-          <div className="w-full bg-[#fff3cd]">
-            <h2 className="text-[#856404] text-xl p-2">Lưu ý Khi đăng tin</h2>
-            <ul className="p-4 text-[#856404] ml-5">
+          <div className="bg-[#fff3cd]">
+            <h2 className="p-2 text-xl text-[#856404]">Lưu ý Khi đăng tin</h2>
+            <ul className="ml-5 text-[#856404]">
               <li>* Nội dung phải viết bằng tiếng Việt có dấu</li>
               <li>*Tiêu đề tin không dài quá 100 kí tự</li>
               <li>
