a7a57232163fe002ae3e3cb70bb34ba64c0e89ab
unknown <mitcoi215>
2023-11-16 03:17:09 +0700

    api delete user

--- src/components/admin/User.jsx
+++ src/components/admin/User.jsx
@@ -2,7 +2,7 @@ import {React, useState, useEffect} from "react";
 import axios from "axios";
 
 const User = () => {
-  const [users, setPost] = useState([]);
+  const [users, setUsers] = useState([]);
   const baseURL = "http://localhost:8080/api/users/users";
   useEffect(() => {
     axios({
@@ -14,25 +14,46 @@ const User = () => {
         "Content-Type": "application/json"
       }
     }).then((response) => {
-      setPost(response.data.listUsers);
+      setUsers(response.data.listUsers);
     });
   }, []);
 
+//edit user
+const [editingUser, setEditingUser] = useState(null);
+const handleEdit = (user) => {
+  setEditingUser(user);
+};
 
-  function deleteid(id){
-    axios({
-      url: "http://localhost:8080/api/users/delete-user/?userId"+id,
-      method: "delete",
-      withCredentials: false,
-      headers: {
-        'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdXkxQGdtYWlsLmNvbSIsInJvbGVzIjpbIkFkbWluIl0sImlhdCI6MTcwMDA2NzUyMSwiZXhwIjoxNzAwMjQwMzIxfQ.gOtjwa6PWQQxzZPCWOzLTVwqMTZBBTI5krhoBpH6lHHvuY6dPYTdtpfUMiAFMIgjbVXpWAc0woAoOvXfvJZ-wA',
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => {
-      setPost(response.data.listUsers);
-    });
-    console.log("aaaa")
-   };
+const handleSave = (editedUser) => {
+  const updatedUsers = users.map((user) =>
+    user.id === editedUser.id ? editedUser : user
+  );
+  setUsers(updatedUsers);
+  setEditingUser(null); // Close the editing window/modal after saving
+};
+
+  //delete user
+  const deleteId = async (id) => {
+    const res = await axios.delete(
+      `http://localhost:8080/api/users/delete-user?userId=${id}`,
+      {
+        withCredentials: false,
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdXkxQGdtYWlsLmNvbSIsInJvbGVzIjpbIkFkbWluIl0sImlhdCI6MTcwMDA2NzUyMSwiZXhwIjoxNzAwMjQwMzIxfQ.gOtjwa6PWQQxzZPCWOzLTVwqMTZBBTI5krhoBpH6lHHvuY6dPYTdtpfUMiAFMIgjbVXpWAc0woAoOvXfvJZ-wA",
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    const userDelete = res.data.user;
+    console.log(userDelete);
+    const newUsers = [...users];
+    newUsers.splice(
+      newUsers.findIndex((user) => user.id === userDelete.id),
+      1,
+    );
+    setUsers(() => newUsers);
+  };
     return (
     <div className="">
       <div>
@@ -60,10 +81,10 @@ const User = () => {
                   <td className="border px-2 flex-1 h-full flex justify-center items-center">{user.phone}</td>
                   <td className="border px-2 flex-1 h-full flex justify-center items-center">{user.address}</td>
                   <td className="border px-2 flex-1 h-full flex items-center justify-center gap-4">
-              <button className="px-2 py-1 text-white bg-green-600 rounded-md hover:underline" >
+              <button className="px-2 py-1 text-white bg-green-600 rounded-md hover:underline" onClick={()=>handleEdit(user.id)} >
                 Sửa
               </button>
-              <button className="px-2 py-1 text-white bg-red-600 rounded-md hover:underline" onClick={deleteid}>
+              <button className="px-2 py-1 text-white bg-red-600 rounded-md hover:underline" onClick={()=>deleteId(user.id)}>
                 Xóa
               </button>
             </td>
@@ -71,6 +92,22 @@ const User = () => {
               ))}
             </tbody>
           </table>
+
+          {editingUser && (
+        <div>
+          <h2>Edit User</h2>
+          <label>Role:</label>
+          <input
+            type="text"
+            value={editingUser.role}
+            onChange={(e) =>
+              setEditingUser({ ...editingUser, name: e.target.value })
+            }
+          />
+          <button className=" border px-3 bg-green-600 rounded-md hover:underline" onClick={() => handleSave(editingUser)}>Save</button>
+        </div>
+      )}
+          
     </div>
         
     )
