cc74b9fa1fdb992b5f0447a360e63b15169c3e88
Xuan Hoang <xuanh6555@gmail.com>
2023-11-18 16:33:57 +0700

    Fix Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 
-import { Home, Spaces, LogIn, Register ,SpaceDetail, Contact, Profile, PostSpace, Messenge, ManagePost, FavoriteSpace,Booking, Sharing } from '../pages/index';
+import { Home, Spaces, LogIn, Register ,SpaceDetail,PostSpaceHome,FavoriteSpace,ManagePostHome,Editprofile,MessengeHome, Contact,Booking, Sharing } from '../pages/index';
 
 import Dashboard from '../components/admin/Dashboard';
 import Owner from '../components/admin/Owner';
 import User from '../components/admin/User';
-import  {LayoutAdmin,LayoutAuth} from "../layouts/index";
+import  {LayoutAdmin,LayoutAuth,LayoutUser} from "../layouts/index";
 
 const router = [
     { path: '/', component: Home },
@@ -15,16 +15,16 @@ const router = [
     { path: '/spaces/:spaceId', component: SpaceDetail },
     { path: '/booking', component: Booking },
     { path: '/sharing', component: Sharing },
-    { path: '/profile',component: Profile },
-    { path: '/post-spaces', component: PostSpace },
-    { path: '/messenger', component: Messenge },
-    { path: '/manage-post', component: ManagePost },
-    { path: '/favorite-space', component: FavoriteSpace },
+    { path: '/profile',layout: LayoutUser,component: Editprofile },
+    { path: '/post-spaces', layout: LayoutUser,component: PostSpaceHome },
+    { path: '/messenger', layout: LayoutUser,component: MessengeHome },
+    { path: '/manage-post', layout: LayoutUser,component: ManagePostHome },
+    { path: '/favorite-space', layout: LayoutUser,component: FavoriteSpace },
     { path: '/spaces/:spaceId', component: SpaceDetail },
     { path: '/admin/dashboard', layout: LayoutAdmin, component: Dashboard},
     { path: '/admin/user', layout: LayoutAdmin, component: User},
     { path: '/admin/owner', layout: LayoutAdmin, component: Owner},
-    { path: '/admin/posts-pace', layout: LayoutAdmin, component: PostSpace}
+    // { path: '/admin/posts-pace', layout: LayoutAdmin, component: PostSpace}
 ];
 
 
